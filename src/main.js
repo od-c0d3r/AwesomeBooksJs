215989dacb2af261891341fc4067a0dff7f3d1a9
od-c0d3r <omar_rashad@aol.com>
2021-11-25 13:35:13 +0200

    Fix: linters passed

--- src/main.js
+++ src/main.js
@@ -1,35 +1,34 @@
-/* eslint-disable import/extensions */
+/* eslint-disable import/extensions, no-undef */
 
 import BookShelf from './modules/bookShelf.js';
 
-var DateTime = luxon.DateTime;
+const { DateTime } = luxon;
 const time = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
-document.getElementById('time').innerHTML = time ;
+document.getElementById('time').innerHTML = time;
 
-
-const listPage = document.getElementById("listPage");
-const formPage = document.getElementById("formPage");
-const contactPage = document.getElementById("contactPage");
+const listPage = document.getElementById('listPage');
+const formPage = document.getElementById('formPage');
+const contactPage = document.getElementById('contactPage');
 
 const app = new BookShelf();
 const form = document.getElementById('form');
 
 function showListPage() {
-  listPage.className = "list-page";
-  formPage.className = "display-none";
-  contactPage.className = "display-none";
+  listPage.className = 'list-page';
+  formPage.className = 'display-none';
+  contactPage.className = 'display-none';
 }
 
 function showFormPage() {
-  listPage.className = "display-none";
-  formPage.className = "form-page";
-  contactPage.className = "display-none";
+  listPage.className = 'display-none';
+  formPage.className = 'form-page';
+  contactPage.className = 'display-none';
 }
 
 function showContactPage() {
-  listPage.className = "display-none";
-  formPage.className = "display-none";
-  contactPage.className = "contact-page";
+  listPage.className = 'display-none';
+  formPage.className = 'display-none';
+  contactPage.className = 'contact-page';
 }
 
 form.addEventListener('submit', (e) => {
@@ -45,14 +44,13 @@ document.addEventListener('click', (e) => {
     const id = e.target.getAttribute('data-id');
     app.removeBook(id);
     e.preventDefault();
-  } else if (e.target.id === "logo") {
-    showListPage()
-  } else if (e.target.id === "listLink") {
-    showListPage()
-  } else if (e.target.id === "formLink") {
-    showFormPage()
-  } else if (e.target.id === "contactLink") {
-    showContactPage()
+  } else if (e.target.id === 'logo') {
+    showListPage();
+  } else if (e.target.id === 'listLink') {
+    showListPage();
+  } else if (e.target.id === 'formLink') {
+    showFormPage();
+  } else if (e.target.id === 'contactLink') {
+    showContactPage();
   }
-
 });
