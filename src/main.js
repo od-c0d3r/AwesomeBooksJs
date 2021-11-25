c385f7961934caca412806d099a36307d3e7ce8d
od-c0d3r <omar_rashad@aol.com>
2021-11-25 12:44:19 +0200

    Feat: Single page appliction pattern implemented

--- src/main.js
+++ src/main.js
@@ -2,9 +2,33 @@
 
 import BookShelf from './modules/bookShelf.js';
 
-const app = new BookShelf();
+const { DateTime } = require("luxon");
 
+const listPage = document.getElementById("listPage");
+const formPage = document.getElementById("formPage");
+const contactPage = document.getElementById("contactPage");
+
+const app = new BookShelf();
 const form = document.getElementById('form');
+
+function showListPage() {
+  listPage.className = "list-page";
+  formPage.className = "display-none";
+  contactPage.className = "display-none";
+}
+
+function showFormPage() {
+  listPage.className = "display-none";
+  formPage.className = "form-page";
+  contactPage.className = "display-none";
+}
+
+function showContactPage() {
+  listPage.className = "display-none";
+  formPage.className = "display-none";
+  contactPage.className = "contact-page";
+}
+
 form.addEventListener('submit', (e) => {
   const title = document.getElementById('titleInput').value;
   const author = document.getElementById('authorInput').value;
@@ -18,7 +42,14 @@ document.addEventListener('click', (e) => {
     const id = e.target.getAttribute('data-id');
     app.removeBook(id);
     e.preventDefault();
+  } else if (e.target.id === "logo") {
+    showListPage()
+  } else if (e.target.id === "listLink") {
+    showListPage()
+  } else if (e.target.id === "formLink") {
+    showFormPage()
+  } else if (e.target.id === "contactLink") {
+    showContactPage()
   }
-  console.log(e.target)
 
 });
